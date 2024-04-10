2c404d260fcfde1585691eede1fab6ad25456425
shyam prakash <shyamprakashhitman45@gmail.com>
2024-04-10 18:10:26 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 function App() {
+  const [val , setVal] = useState([1,2,3,4,5,6,])
   return (
-    <div>
-      
+    <div className='p-5'>
+      {val.map(item=><h1>{item}</h1>)}
     </div>
   )
 }
